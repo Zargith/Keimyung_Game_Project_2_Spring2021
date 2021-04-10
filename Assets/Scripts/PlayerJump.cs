8324769e0d69e5d2ec36119fca4b37c91fdc667e
Cavalli Timothée <35866375+NoobyBoy@users.noreply.github.com>
2021-04-10 18:37:05 +0900

    Jump scene & script

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool isGrounded;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask layerForCheck;
+    
+    public float jumpForce = 5.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, layerForCheck);
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+}
