8c3e45a25eb7d35a1045162d2727bb56c584eac0
Zargith <stanislas.hegron@gmail.com>
2021-04-13 21:02:33 +0900

    Added text that appears and desappears on tuto scene

--- Assets/Scripts/Firefly/FireflyDecreaseLight.cs
+++ Assets/Scripts/Firefly/FireflyDecreaseLight.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class FireflyDecreaseLight : MonoBehaviour
 {
-	private Light light;
+	private Light _light;
 	public float minIntensity = 0.0f;
-	public float maxIntensity = 1.5f;	
+	public float maxIntensity = 1.5f;
 	public float frequency = 0.01f;
-	public float phase = 0.0f;
+	private bool increasing = true;
+	private float intensity = 0f;
 
 	void Start () 
 	{
-		light = GetComponent<Light>();
+		_light = GetComponent<Light>();
+		_light.intensity = intensity;
 	}
 	
 	void Update () 
 	{
-		float x = (Time.time + phase) * frequency;
-		x = x - Mathf.Floor(x);
-		light.intensity = maxIntensity * Mathf.Sin(2 * Mathf.PI * x) + minIntensity;
+		if (increasing) {
+			intensity += frequency * Time.deltaTime;
+			if (intensity >= maxIntensity)
+				increasing = false;
+		} else {
+			intensity -= frequency * Time.deltaTime;
+			if (intensity <= minIntensity)
+				increasing = true;
+		}
+
+		_light.intensity = intensity;
 	}
 }
---
+++ Assets/Scripts/interactions/DisplayText1TimeWhenTrigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayText1TimeWhenTrigger : MonoBehaviour
+{
+    private MeshRenderer mesh;
+    private bool hasAlreadyTriggered = false;
+    private TextMesh text;
+    private float alpha = 0;
+
+    void Start() {
+        mesh = GetComponent<MeshRenderer>();
+        text = GetComponent<TextMesh>();
+        var matrialColor = text.color;
+        text.color = new Color(matrialColor.r, matrialColor.g, matrialColor.b, 0);
+    }
+
+    void Update()
+    {
+        if (mesh.enabled && !hasAlreadyTriggered && alpha < 100)
+            alpha++;
+        else if (mesh.enabled && hasAlreadyTriggered) {
+            if (alpha > 0)
+                alpha--;
+            else
+                mesh.enabled = false;
+        }
+
+        var color = text.color;
+        text.color = new Color(color.r, color.g, color.b, alpha/100);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!hasAlreadyTriggered && other.gameObject.CompareTag("Player"))
+            mesh.enabled = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (!hasAlreadyTriggered && other.gameObject.CompareTag("Player"))
+            hasAlreadyTriggered = true;
+    }
+}
