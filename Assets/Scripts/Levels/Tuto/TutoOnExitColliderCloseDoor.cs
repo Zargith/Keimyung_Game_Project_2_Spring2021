78e82e03e12d8e580d236b09aab1a93de505aab5
Zargith <stanislas.hegron@gmail.com>
2021-04-21 22:19:37 +0900

    Tuto: added pause system and close door at the end of the level

---
+++ Assets/Scripts/Levels/Tuto/TutoOnExitColliderCloseDoor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoOnExitColliderCloseDoor : MonoBehaviour
+{
+    AudioSource _audioSource;
+    BoxCollider2D _boxCollider;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player") && other.transform.position.x > transform.position.x) {
+            _audioSource.Play();
+            _boxCollider.isTrigger = false;
+        }
+    }
+}
