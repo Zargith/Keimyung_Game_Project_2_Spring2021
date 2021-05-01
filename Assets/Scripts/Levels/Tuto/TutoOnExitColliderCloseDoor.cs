cd05bbef0bbd234ff9d9637f3446d7c8b22a4535
Zargith <stanislas.hegron@gmail.com>
2021-05-01 22:19:54 +0900

    Added and updated some sprites for room and upgraded tuto scene

--- Assets/Scripts/Levels/Tuto/TutoOnExitColliderCloseDoor.cs
+++ Assets/Scripts/Levels/Tuto/TutoOnExitColliderCloseDoor.cs
@@ -6,6 +6,8 @@ public class TutoOnExitColliderCloseDoor : MonoBehaviour
 {
 	AudioSource _audioSource;
 	BoxCollider2D _boxCollider;
+	[SerializeField] GameObject outsideDreamRoom;
+	[SerializeField] SpriteRenderer fournituresToChangeOrderInLayer;
 
 	void Start()
 	{
@@ -17,6 +19,8 @@ public class TutoOnExitColliderCloseDoor : MonoBehaviour
 		if (other.gameObject.CompareTag("Player") && other.transform.position.x > transform.position.x) {
 			_audioSource.Play();
 			_boxCollider.isTrigger = false;
+			outsideDreamRoom.SetActive(false);
+			fournituresToChangeOrderInLayer.sortingOrder = 1;
 		}
 	}
 }
