b58eaa360157b7ffab058fffe9c0994e9c6b8f14
Raykuzu <etienne.g35@gmail.com>
2021-05-12 17:19:40 +0900

    create true level manager with inputs + turn system

--- Assets/Scripts/Levels/Mysophobia/LevelManager.cs
+++ Assets/Scripts/Levels/Mysophobia/LevelManager.cs
@@ -4,32 +4,81 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    bool quit;
+    private bool quit;
 
-    MapProvider mapProvider;
+    private MapProvider mapProvider;
 
-    Board board;
+    private Board board;
 
-    Environment environment;
+    private Environment environment;
+
+    [SerializeField] private string mapName;
+
+    int turn;
 
     // Start is called before the first frame update
     void Start()
     {
         quit = false;
 
+        turn = 0;
+
         mapProvider = ScriptableObject.CreateInstance<MapProvider>();
 
-        board = GameObject.Find("Board").GetComponent<Board>();
-        environment = GameObject.Find("Environment").GetComponent<Environment>();
+        //board = GameObject.Find("Board").GetComponent<Board>();
+        //environment = GameObject.Find("Environment").GetComponent<Environment>();
 
-        launchGame("map_1");
+        launchGame(mapName);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (turn == 0) 
+        {
+            playerTurn();
+        } else
+        {
+            virusTurn();
+        }
+
+            
+        // Input
+        // Activated power ?
+        // Wait move
+        // Virus turn
+        // Pop back queue
+        // Visual env effect
+        // Apply (virus pop or not)
+        // Visual maj queue
+        // decrease max turn (?)
+        // next turn
     }
+
+    private void playerTurn()
+    {
+        bool ret = false;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            ret = board._player.Move(Player.Movement.DOWN);
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            ret = board._player.Move(Player.Movement.UP);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            ret = board._player.Move(Player.Movement.LEFT);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            ret = board._player.Move(Player.Movement.RIGHT);
+        if (ret)
+        {
+            turn = 1;
+        }
+    }
+    private void virusTurn()
+    {
+        Debug.Log("Ennemy turn");
+        turn = 0;
+    }
+
+
 
     public void launchGame(string mapName)
     {
@@ -43,8 +92,8 @@ public class LevelManager : MonoBehaviour
 
         pp = new PositionProvider(new Vector2(0, 0), new Vector2Int(mapProvider.RowsLength, mapProvider.ColumnsLength), 1); // tmp
 
-        board.SetupPositionProvider(pp);
-        environment.SetupPositionProvider(pp);
+        board = new Board(pp);
+        environment = new Environment(pp);
 
         /*LINK GRAPHIC AND INTERN*/
 
@@ -55,30 +104,6 @@ public class LevelManager : MonoBehaviour
         board.Draw();
         environment.Draw();
 
-        /*ADDITIONALS INIT*/
-
-        board.playerSpawn();
         //Init action queue
-
-        /*GAME LOOP*/
-
-        gameLoop();
-    }
-
-    private void gameLoop()
-    {
-        while (!quit)
-        {
-            // Input
-            // Activated power ?
-            // Wait move
-            // Virus turn
-            // Pop back queue
-            // Visual env effect
-            // Apply (virus pop or not)
-            // Visual maj queue
-            // decrease max turn (?)
-            // next turn
-        }
     }
 }
