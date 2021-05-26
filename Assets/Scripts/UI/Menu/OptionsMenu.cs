e24c9e2e8800d41a6e3e0d64a6f7ac36b02941ba
Zargith <stanislas.hegron@gmail.com>
2021-05-26 22:30:06 +0900

    PauseMenu: Added sound sliders for master, musics and soundeffects

--- Assets/Scripts/UI/Menu/OptionsMenu.cs
+++ Assets/Scripts/UI/Menu/OptionsMenu.cs
@@ -7,24 +7,15 @@ using UnityEngine.UI;
 public class OptionsMenu : MonoBehaviour
 {
     public TMPro.TMP_Dropdown resolutionDropDown;
-
     public TMPro.TMP_Dropdown screenModeDropDown;
 
     int resolutionX = 1280;
-
     int resolutionY = 720;
-
     bool fullscreen = false;
 
-
-    public Slider musicVolumeSlider;
-
-    public AudioMixer mixer;
     public void OnResolutionChange() 
     {
         string value = resolutionDropDown.options[resolutionDropDown.value].text;
-        Debug.Log("OptionsMenu - OnResolutionChange: " + value);
-
         string[] resolutions = value.Split('x');
 
         resolutionX = int.Parse(resolutions[0]);
@@ -35,19 +26,11 @@ public class OptionsMenu : MonoBehaviour
     public void OnScreenModeChange()
     {
         string value = screenModeDropDown.options[screenModeDropDown.value].text;
-        Debug.Log("OptionsMenu - OnScreenModeChange: " + value);
 
         if (value == "FullScreen")
             fullscreen = true;
         else
             fullscreen = false;
         Screen.SetResolution(resolutionX, resolutionY, fullscreen);
-    }
-
-    public void OnVolumeChange()
-    {
-        float value = musicVolumeSlider.value;
-        Debug.Log("OptionsMenu - OnVolumeChange: " + value);
-        mixer.SetFloat("MusicVolume", value);
     }
 }
