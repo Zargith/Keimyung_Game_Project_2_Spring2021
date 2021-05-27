fc4f0b72c1d7cb219043bb45888063ef41dbd4ef
Zargith <stanislas.hegron@gmail.com>
2021-05-28 00:41:44 +0900

    Fixed option menu changing color of other buttons and fixef bad layer order

--- Assets/Scripts/UI/Menu/DisableMenuButton.cs
+++ Assets/Scripts/UI/Menu/DisableMenuButton.cs
@@ -8,14 +8,17 @@ public class DisableMenuButton : MonoBehaviour
     [SerializeField] List<Button> buttons;
     [SerializeField] GameObject menu;
     [SerializeField] bool pastState;
-
+    [SerializeField] Color enabledButtonColor;
+    [SerializeField] Color disabledButtonColor;
 
     void Update()
     {
         if (pastState != menu.activeSelf) {
             pastState = !pastState;
-            for (int i = 0 ; i < buttons.Count ; i++)
+            for (int i = 0 ; i < buttons.Count ; i++) {
                 buttons[i].enabled = !pastState;
+                buttons[i].GetComponent<Image>().color = buttons[i].enabled ? enabledButtonColor : disabledButtonColor;
+            }
         }
     }
 
