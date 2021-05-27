6a9300e34b20721a5509fe7722d3a644094dac44
Zargith <stanislas.hegron@gmail.com>
2021-05-27 19:54:55 +0900

    FIX: Fixed time stop on changing scene from pause menu

--- Assets/Scripts/UI/Menu/StartMenuUiEffect.cs
+++ Assets/Scripts/UI/Menu/StartMenuUiEffect.cs
@@ -19,6 +19,7 @@ public class StartMenuUiEffect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
