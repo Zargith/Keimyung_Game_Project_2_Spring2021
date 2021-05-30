1f424bf88e040ac21bb74b613b460cda575f8ca8
f_chavagnieux <florian.chavagnieux@epitech.eu>
2021-05-30 23:35:30 +0900

    OthersFear: Fix unset elements, puzzle scale

--- Assets/Scripts/Levels/OthersFear/Puzzles/Puzzle1.cs
+++ Assets/Scripts/Levels/OthersFear/Puzzles/Puzzle1.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Puzzle1 : MonoBehaviour
 {
     [SerializeReference] GameObject reward;
+    [SerializeField] Vector2 posReward;
     [SerializeReference] List<Puzzle1_Lever> triggers;
 
     // Start is called before the first frame update
@@ -30,9 +31,9 @@ public class Puzzle1 : MonoBehaviour
             reward.transform.rotation = Quaternion.Euler(0, 0, 0);
             reward.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
             reward.GetComponent<Collider2D>().enabled = false;
-            reward.transform.localPosition = new Vector3(reward.transform.localPosition.x, -4.5f, 0);
+            reward.transform.position = posReward;
             var tmp = reward.GetComponentInChildren<OthersFear_Item>().gameObject;
-            tmp.transform.localScale = new Vector3(1, 1, 1);
+            tmp.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, 1);
             tmp.GetComponentInChildren<UnityEngine.Experimental.Rendering.Universal.Light2D>().pointLightOuterRadius = 1.25f;
         }
     }
