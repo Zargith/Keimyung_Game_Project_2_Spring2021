36fd4799a870f3b417a3637432fcce02276b03d3
Raykuzu <etienne.g35@gmail.com>
2021-05-31 21:46:34 +0900

    add tuto (press h for help)

--- Assets/Scripts/Levels/Mysophobia/Menu/MysophobiaMenuManager.cs
+++ Assets/Scripts/Levels/Mysophobia/Menu/MysophobiaMenuManager.cs
@@ -4,39 +4,65 @@ public class MysophobiaMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject MysophobiaMenu;
 
-    private bool _active;
+    [SerializeField] private GameObject GameCanvas;
+
+    [SerializeField] private GameObject TutoCanvas;
+
+    private bool _menuActive;
+
+    private bool _tutoActive;
 
     // Update is called once per frame
     void Update()
     {
-        if (!MysophobiaMenu.activeSelf && _active)
-            Unpause();
+        if (!MysophobiaMenu.activeSelf && _menuActive)
+            HideMenu();
         if (Input.GetKeyDown(KeyCode.F1))
         {
-            if (_active)
-                Unpause();
+            if (_menuActive)
+                HideMenu();
             else
-                Pause();
+                LaunchMenu();
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (_tutoActive)
+                HideTuto();
+            else
+                LaunchTuto();
         }
 
-        if (_active)
+        if (_menuActive)
             return;
     }
 
-    void Pause()
+    void LaunchTuto()
+    {
+        Time.timeScale = 0;
+        TutoCanvas.SetActive(true);
+        _tutoActive = true;
+    }
+
+    void HideTuto()
+    {
+        Time.timeScale = 1;
+        TutoCanvas.SetActive(false);
+        _tutoActive = false;
+    }
+    void LaunchMenu()
     {
         Time.timeScale = 0;
         FindInactiveChild("GameModeMenu").SetActive(true);
         FindInactiveChild("FreeplayMenu").SetActive(false);
         MysophobiaMenu.SetActive(true);
-        _active = true;
+        _menuActive = true;
     }
 
-    void Unpause()
+    void HideMenu()
     {
         Time.timeScale = 1;
         MysophobiaMenu.SetActive(false);
-        _active = false;
+        _menuActive = false;
     }
 
     public void DisplayFreeplayMenu()
@@ -44,7 +70,7 @@ public class MysophobiaMenuManager : MonoBehaviour
         FindInactiveChild("GameModeMenu").SetActive(false);
         FindInactiveChild("FreeplayMenu").SetActive(true);
         MysophobiaMenu.SetActive(true);
-        _active = true;
+        _menuActive = true;
     }
 
     private GameObject FindInactiveChild(string name)
